bee81a620e26e4f196f89c4b698fbc8797a63e9d
Hamidov2004 <ismoilxojahamidov@gmail.com>
2024-11-28 20:56:12 +0500

    Add .gitattributes for line-ending normalization

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import './App.css'
-import { Button } from "@/components/ui/button"
+
 
 function App() {
 
 
   return (
     <>
-        <h1 className='text-red-600 font-semibold text-4xl text-center'>
-              Project
-        </h1>
-         <Button>Click me</Button>
+        <div>
+          App
+        </div>
     </>
   )
 }
