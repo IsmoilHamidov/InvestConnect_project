a959da01579ff482e9e3c324d0b2cf1c0d19da2f
Hamidov2004 <ismoilxojahamidov@gmail.com>
2024-11-28 21:41:47 +0500

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { router } from './Router/Router'
 function App() {
   return (
       <div>
-          <h1>App</h1>
+          <h1>Apps</h1>
           <RouterProvider router={router}/>
       </div>
   )
