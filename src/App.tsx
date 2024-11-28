8ffede05c5d48701ae59e59cba2f585ea44a14cd
Hamidov2004 <ismoilxojahamidov@gmail.com>
2024-11-28 21:15:26 +0500

    Normalize line endings

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import './App.css'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './Router/Router'
 
 
 function App() {
-
-
   return (
-    <>
-        <div>
-          App
-        </div>
-    </>
+      <div>
+          <RouterProvider router={router}/>
+      </div>
   )
 }
 
