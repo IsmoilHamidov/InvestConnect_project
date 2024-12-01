60ea247521db78fc1ded78b6013693edaa8718d3
to'raboyev xursand <xursandtoraboyev810@gmail.com>
2024-12-02 00:10:57 +0500

    Xursand first commit

---
+++ src/Pages/profile/index.tsx
@@ -0,0 +1,41 @@
+import NavbarAdmin from "@/components/navbarAdmin";
+import Sidebar from "@/components/sidebar";
+import { ProfileForm } from "@/components/profileForms";
+import Offers from "../../components/OffersCard";
+import TimeCount from "../../components/timeCount";
+import Activities from "../../components/Activities";
+
+const Profile = () => {
+  return (
+    <div className="flex flex-col w-full h-screen">
+      <header className="w-full relative">
+        <NavbarAdmin />
+      </header>
+      <div className="flex w-full flex-1 relative">
+        <aside className="w-[270px] bg-white border relative">
+          <Sidebar />
+        </aside>
+        <main className="flex-1 relative pt-10 pl-6 pr-9">
+          <div className="flex mt-12 gap-x-8">
+            <div className="w-full">
+              <ProfileForm />
+            </div>
+            <div className="flex flex-col w-full gap-y-7">
+              <div className="w-full">
+                <TimeCount />
+              </div>
+              <div>
+                <Offers />
+              </div>
+              <div className="w-full">
+                <Activities />
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
---
+++ src/components/OffersCard/index.tsx
@@ -0,0 +1,102 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { OfferCardProps } from "@/interface";
+import { Link } from "react-router-dom"
+
+const ProperyCard: React.FC<OfferCardProps> = ({
+  image,
+  title,
+  location,
+  type,
+  price,
+  isTopLocation,
+}) => {
+  return (
+    <Card className="flex relative border-[#C5C3C3]  items-center p-4 mb-4">
+      <img
+        src={image}
+        alt={title}
+        className="w-24 h-24 rounded object-cover mr-4"
+      />
+      <div className="flex-1">
+
+        <CardHeader className="p-0">
+          <CardTitle className="text-lg font-bold text-[#749BA9] uppercase">
+            {title}
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="p-0 flex flex-row items-center gap-2 mt-1">
+          <div  >
+            <p className="text-sm text-[#948F8F] font-light ">
+              {location} · <span className="font-normal text-[#443B3A] text-xs  ">{type}</span>
+            </p>
+          </div>
+          <div>
+
+            <p className="text-base font-semibold mt-2">{price}</p>
+          </div>
+        </CardContent>
+      </div>
+      {isTopLocation && (
+        <div className="bg-[#B29C6F] absolute top-0 right-0   px-3 py-1">
+          <p className="font-normal text-white text-[10px] leading-3 " >
+            Topligging
+          </p>
+        </div>
+      )}
+      <Button variant="outline" className=" bg-[#749BA9] rounded-[40px] text-xs text-white ">
+        Ontdek meer!
+      </Button>
+    </Card>
+  );
+};
+
+const Offers: React.FC = () => {
+  const offers = [
+    {
+      image: "https://via.placeholder.com/150",
+      title: "Machelen",
+      location: "City Gate",
+      type: "Apartment",
+      price: "Vanaf € 160 000",
+      isTopLocation: true,
+    },
+    {
+      image: "https://via.placeholder.com/150",
+      title: "Machelen",
+      location: "City Gate",
+      type: "Apartment",
+      price: "Vanaf € 160 000",
+      isTopLocation: true,
+    },
+    {
+      image: "https://via.placeholder.com/150",
+      title: "Machelen",
+      location: "City Gate",
+      type: "Apartment",
+      price: "Vanaf € 160 000",
+      isTopLocation: true,
+    },
+  ];
+
+  return (
+    <div className="p-6 w-full mx-auto shadow-xl border-[1px]  rounded-[15px]  ">
+      <h1 className="text-base font-semibold mb-4">Aanbod</h1>
+      {offers.map((offer, index) => (
+        <ProperyCard key={index} {...offer} />
+      ))}
+      <div className="flex justify-end" >
+
+        <Link
+          to={"#"}
+          className=" text-end  text-[#443B3A] font-normal hover:underline"
+        >
+          Het aanbod
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Offers;
